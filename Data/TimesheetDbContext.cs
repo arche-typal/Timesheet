f6771b5745331faf43e695a720ed0c1bc7ae63f6
Archetypal <archetypal.0@gmail.com>
2020-01-25 08:36:02 -0600

    Add project files.

---
+++ Data/TimesheetDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using timesheet.Models;
+
+namespace timesheet.Data
+{
+    public class TimesheetDbContext : DbContext
+    {
+        public DbSet<Timesheet> Timesheets { get; set; }
+
+        public TimesheetDbContext(DbContextOptions<TimesheetDbContext> options)
+            : base(options)
+        { }
+    }
+}
